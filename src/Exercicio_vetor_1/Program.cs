a8d3ba6a3fdddb10ba6b3eb7128be98b8aa2ec7b
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:15:12 -0300

    Program.cs adicionado

---
+++ src/Exercicio_vetor_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_vetor_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
